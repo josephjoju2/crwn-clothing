19f5bffd410bc8e1efc9885f9c29313e1e30234d
joseph <josephjoju2@gmail.com>
2021-08-01 22:43:30 +0530

    finishing sign in handle submit method

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
                 id:snapShot.id,
                 ...snapShot.data()
               }
-            })
+            });
         })
       }
       else{this.setState({currentUser:userAuth});}
